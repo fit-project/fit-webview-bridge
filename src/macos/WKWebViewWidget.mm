e5461663ef68411bb3a4e4289ff50b17d01bc1c0
Fabio Zito <zitelog@gmail.com>
2025-11-01 23:09:46 +0100

    chore: bump version to 0.2.0a4

--- src/macos/WKWebViewWidget.mm
+++ src/macos/WKWebViewWidget.mm
@@ -8,10 +8,13 @@
 #include <QUrl>
 
 @class WKNavDelegate;
+@class FitUrlMsgHandler;
 
 struct WKWebViewWidget::Impl {
-    WKWebView*     wk       = nil;
-    WKNavDelegate* delegate = nil;
+    WKWebView*        wk        = nil;
+    WKNavDelegate*    delegate  = nil;
+    WKUserContentController* ucc = nil;
+    FitUrlMsgHandler* msg       = nil;
 };
 
 @interface WKNavDelegate : NSObject <WKNavigationDelegate>
@@ -19,11 +22,34 @@ struct WKWebViewWidget::Impl {
 @end
 
 @implementation WKNavDelegate
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    if (!self.owner) return;
+    if (webView.URL)
+        emit self.owner->urlChanged(QUrl::fromEncoded(QByteArray(webView.URL.absoluteString.UTF8String)));
+    emit self.owner->loadProgress(5);
+    emit self.owner->canGoBackChanged(webView.canGoBack);
+    emit self.owner->canGoForwardChanged(webView.canGoForward);
+}
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+    if (!self.owner) return;
+    if (webView.URL)
+        emit self.owner->urlChanged(QUrl::fromEncoded(QByteArray(webView.URL.absoluteString.UTF8String)));
+    emit self.owner->loadProgress(50);
+    emit self.owner->canGoBackChanged(webView.canGoBack);
+    emit self.owner->canGoForwardChanged(webView.canGoForward);
+}
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation {
+    if (!self.owner) return;
+    if (webView.URL)
+        emit self.owner->urlChanged(QUrl::fromEncoded(QByteArray(webView.URL.absoluteString.UTF8String)));
+}
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
     if (!self.owner) return;
     emit self.owner->loadFinished(true);
-    if (webView.URL) emit self.owner->urlChanged(QUrl::fromEncoded(QByteArray(webView.URL.absoluteString.UTF8String)));
-    if (webView.title) emit self.owner->titleChanged(QString::fromUtf8(webView.title.UTF8String));
+    if (webView.URL)
+        emit self.owner->urlChanged(QUrl::fromEncoded(QByteArray(webView.URL.absoluteString.UTF8String)));
+    if (webView.title)
+        emit self.owner->titleChanged(QString::fromUtf8(webView.title.UTF8String));
     emit self.owner->loadProgress(100);
     emit self.owner->canGoBackChanged(webView.canGoBack);
     emit self.owner->canGoForwardChanged(webView.canGoForward);
@@ -35,20 +61,32 @@ struct WKWebViewWidget::Impl {
     emit self.owner->canGoBackChanged(webView.canGoBack);
     emit self.owner->canGoForwardChanged(webView.canGoForward);
 }
-- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
-    if (self.owner) emit self.owner->loadProgress(5);
-}
-- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
-    if (self.owner) emit self.owner->loadProgress(50);
+@end
+
+// Handler per messaggi JS (SPA: pushState/replaceState/popstate/click)
+@interface FitUrlMsgHandler : NSObject <WKScriptMessageHandler>
+@property(nonatomic, assign) WKWebViewWidget* owner;
+@end
+
+@implementation FitUrlMsgHandler
+- (void)userContentController:(WKUserContentController *)userContentController
+      didReceiveScriptMessage:(WKScriptMessage *)message {
+    if (!self.owner) return;
+    if (![message.name isEqualToString:@"fitUrlChanged"]) return;
+    if (![message.body isKindOfClass:[NSString class]]) return;
+    QString s = QString::fromUtf8([(NSString*)message.body UTF8String]);
+    emit self.owner->urlChanged(QUrl::fromEncoded(s.toUtf8()));
 }
 @end
 
+// Conversione QUrl -> NSURL con forzatura http->https e normalizzazione user-input
 static NSURL* toNSURL(QUrl u) {
     if (!u.isValid()) return nil;
 
     if (u.scheme().isEmpty())
         u = QUrl::fromUserInput(u.toString());
 
+    // Forza sempre http -> https (nessuna eccezione)
     if (u.scheme() == "http")
         u.setScheme("https");
 
@@ -58,7 +96,6 @@ static NSURL* toNSURL(QUrl u) {
     const QByteArray enc = u.toString(QUrl::FullyEncoded).toUtf8();
     return [NSURL URLWithString:[NSString stringWithUTF8String:enc.constData()]];
 }
-
 
 WKWebViewWidget::WKWebViewWidget(QWidget* parent)
     : QWidget(parent), d(new Impl) {
@@ -70,6 +107,39 @@ WKWebViewWidget::WKWebViewWidget(QWidget* parent)
     if ([cfg respondsToSelector:@selector(defaultWebpagePreferences)]) {
         cfg.defaultWebpagePreferences.allowsContentJavaScript = YES;
     }
+
+    // UserContentController + script per intercettare navigazioni SPA
+    d->ucc = [WKUserContentController new];
+    d->msg = [FitUrlMsgHandler new];
+    d->msg.owner = this;
+    [d->ucc addScriptMessageHandler:d->msg name:@"fitUrlChanged"];
+
+    NSString* js =
+        @"(function(){"
+        @"  function emit(){"
+        @"    try{ window.webkit.messageHandlers.fitUrlChanged.postMessage(location.href); }catch(e){}"
+        @"  }"
+        @"  var _ps = history.pushState;"
+        @"  history.pushState = function(){ _ps.apply(this, arguments); emit(); };"
+        @"  var _rs = history.replaceState;"
+        @"  history.replaceState = function(){ _rs.apply(this, arguments); emit(); };"
+        @"  window.addEventListener('popstate', emit, true);"
+        @"  document.addEventListener('click', function(ev){"
+        @"    var a = ev.target && ev.target.closest ? ev.target.closest('a[href]') : null;"
+        @"    if (!a) return;"
+        @"    if (a.target === '_blank' || a.hasAttribute('download')) return;"
+        @"    setTimeout(emit, 0);"  // lascia tempo alla SPA di aggiornare l’URL
+        @"  }, true);"
+        @"})();";
+
+    WKUserScript* us = [[WKUserScript alloc]
+        initWithSource:js
+        injectionTime:WKUserScriptInjectionTimeAtDocumentStart
+        forMainFrameOnly:YES];
+
+    [d->ucc addUserScript:us];
+    cfg.userContentController = d->ucc;
+
     d->wk = [[WKWebView alloc] initWithFrame:nsParent.bounds configuration:cfg];
     d->wk.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
     [nsParent addSubview:d->wk];
@@ -81,8 +151,15 @@ WKWebViewWidget::WKWebViewWidget(QWidget* parent)
 
 WKWebViewWidget::~WKWebViewWidget() {
     if (!d) return;
+    if (d->ucc && d->msg) {
+        @try { [d->ucc removeScriptMessageHandlerForName:@"fitUrlChanged"]; } @catch (...) {}
+    }
+    d->msg = nil;
+
     if (d->wk) { [d->wk removeFromSuperview]; d->wk = nil; }
     d->delegate = nil;
+    d->ucc = nil;
+
     delete d; d = nullptr;
 }
 
